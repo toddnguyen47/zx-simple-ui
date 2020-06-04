a63c8b9e7ebabccdea81ca4b7469d79770b1af0f
Todd Nguyen <todd.nguyen47@gmail.com>
2020-06-04 02:48:45 -0700

    Finish refactoring PlayerPower47.lua

--- modules/player/PlayerPower47.lua
+++ modules/player/PlayerPower47.lua
@@ -1,66 +1,68 @@
+-- #region
 --- upvalues to prevent warnings
 local LibStub = LibStub
-local UnitPower, UnitPowerMax = UnitPower, UnitPowerMax
-local UnitClass, UnitPowerType = UnitClass, UnitPowerType
+local UnitPower, UnitPowerMax, UnitPowerType = UnitPower, UnitPowerMax, UnitPowerType
 
 --- include files
 local ZxSimpleUI = LibStub("AceAddon-3.0"):GetAddon("ZxSimpleUI")
+local Utils47 = ZxSimpleUI.Utils47
 local BarTemplateDefaults = ZxSimpleUI.prereqTables["BarTemplateDefaults"]
 local BarTemplate = ZxSimpleUI.prereqTables["BarTemplate"]
-local Utils47 = ZxSimpleUI.Utils47
 local RegisterWatchHandler47 = ZxSimpleUI.RegisterWatchHandler47
 
-local _MODULE_NAME = "PlayerPower47"
-local _DECORATIVE_NAME = "Player Power"
-local PlayerPower47 = ZxSimpleUI:NewModule(_MODULE_NAME)
+local MODULE_NAME = "PlayerPower47"
+local DECORATIVE_NAME = "Player Power"
+local PlayerPower47 = ZxSimpleUI:NewModule(MODULE_NAME)
 
-PlayerPower47.MODULE_NAME = _MODULE_NAME
-PlayerPower47.DECORATIVE_NAME = _DECORATIVE_NAME
+PlayerPower47.MODULE_NAME = MODULE_NAME
+PlayerPower47.DECORATIVE_NAME = DECORATIVE_NAME
+PlayerPower47.bars = nil
 PlayerPower47.unit = "player"
-
-local _powerEventColorTable = {
-  ["UNIT_MANA"] = {0.0, 0.0, 1.0, 1.0},
-  ["UNIT_RAGE"] = {1.0, 0.0, 0.0, 1.0},
-  ["UNIT_FOCUS"] = {1.0, 0.65, 0.0, 1.0},
-  ["UNIT_ENERGY"] = {1.0, 1.0, 0.0, 1.0},
-  ["UNIT_RUNIC_POWER"] = {0.0, 1.0, 1.0, 1.0}
-}
-
-local _unitPowerTypeTable = {
-  ["MANA"] = 0,
-  ["RAGE"] = 1,
-  ["FOCUS"] = 2,
-  ["ENERGY"] = 3,
-  ["COMBOPOINTS"] = 4,
-  ["RUNES"] = 5,
-  ["RUNICPOWER"] = 6
-}
-
-local _defaults = {
-  profile = {
-    enabledToggle = true,
-    showbar = false,
-    width = 200,
-    height = 26,
-    xoffset = 400,
-    yoffset = 240,
-    fontsize = 16,
-    font = "PT Sans Bold",
-    fontcolor = {1.0, 1.0, 1.0},
-    texture = "Skewed",
-    color = _powerEventColorTable["UNIT_MANA"], -- need this option for createBar() to work
-    colorMana = _powerEventColorTable["UNIT_MANA"],
-    colorRage = _powerEventColorTable["UNIT_RAGE"],
-    colorFocus = _powerEventColorTable["UNIT_FOCUS"],
-    colorEnergy = _powerEventColorTable["UNIT_ENERGY"],
-    colorRunicPower = _powerEventColorTable["UNIT_RUNIC_POWER"],
-    border = "None"
-  }
-}
+-- #endregion
 
 function PlayerPower47:__init__()
+  self._powerEventColorTable = {
+    ["UNIT_MANA"] = {0.0, 0.0, 1.0, 1.0},
+    ["UNIT_RAGE"] = {1.0, 0.0, 0.0, 1.0},
+    ["UNIT_FOCUS"] = {1.0, 0.65, 0.0, 1.0},
+    ["UNIT_ENERGY"] = {1.0, 1.0, 0.0, 1.0},
+    ["UNIT_RUNIC_POWER"] = {0.0, 1.0, 1.0, 1.0}
+  }
+
+  self._unitPowerTypeTable = {
+    ["MANA"] = 0,
+    ["RAGE"] = 1,
+    ["FOCUS"] = 2,
+    ["ENERGY"] = 3,
+    ["COMBOPOINTS"] = 4,
+    ["RUNES"] = 5,
+    ["RUNICPOWER"] = 6
+  }
+
+  self._defaults = {
+    profile = {
+      enabledToggle = true,
+      showbar = false,
+      width = 200,
+      height = 26,
+      xoffset = 400,
+      yoffset = 240,
+      fontsize = 16,
+      font = "PT Sans Bold",
+      fontcolor = {1.0, 1.0, 1.0},
+      texture = "Skewed",
+      color = self._powerEventColorTable["UNIT_MANA"], -- need this option for createBar() to work
+      colorMana = self._powerEventColorTable["UNIT_MANA"],
+      colorRage = self._powerEventColorTable["UNIT_RAGE"],
+      colorFocus = self._powerEventColorTable["UNIT_FOCUS"],
+      colorEnergy = self._powerEventColorTable["UNIT_ENERGY"],
+      colorRunicPower = self._powerEventColorTable["UNIT_RUNIC_POWER"],
+      border = "None"
+    }
+  }
+
   self.mainFrame = nil
-  self.currentPowerColorEdited = _powerEventColorTable["UNIT_MANA"]
+  self.currentPowerColorEdited = self._powerEventColorTable["UNIT_MANA"]
 
   self._timeSinceLastUpdate = 0
   self._prevPowerValue = UnitPowerMax(self.unit)
@@ -70,34 +72,35 @@ function PlayerPower47:__init__()
 
   self._barTemplateDefaults = BarTemplateDefaults:new()
   self._newDefaults = self._barTemplateDefaults.defaults
-  Utils47:replaceTableValue(self._newDefaults.profile, _defaults.profile)
+  Utils47:replaceTableValue(self._newDefaults.profile, self._defaults.profile)
 end
 
+---Do init tasks here, like loading the Saved Variables,
+---Or setting up slash commands.
 function PlayerPower47:OnInitialize()
   self:__init__()
-
-  self.db = ZxSimpleUI.db:RegisterNamespace(_MODULE_NAME, self._newDefaults)
+  self.db = ZxSimpleUI.db:RegisterNamespace(self.MODULE_NAME, self._newDefaults)
   self._curDbProfile = self.db.profile
+
   -- Always set the showbar option to false on initialize
-  self._curDbProfile.showbar = _defaults.profile.showbar
+  self._curDbProfile.showbar = self._defaults.profile.showbar
 
   self.bars = BarTemplate:new(self.db)
-
-  self:SetEnabledState(ZxSimpleUI:getModuleEnabledState(_MODULE_NAME))
+  self:SetEnabledState(ZxSimpleUI:getModuleEnabledState(self.MODULE_NAME))
 end
 
+---Do more initialization here, that really enables the use of your addon.
+---Register Events, Hook functions, Create Frames, Get information from
+---the game that wasn't available in OnInitialize
 function PlayerPower47:OnEnable()
-  if self.mainFrame ~= nil then
-    self:refreshConfig()
-    self.mainFrame:Show()
-  end
+  if self.mainFrame == nil then self:createBar() end
+  self.mainFrame:Show()
 end
 
-function PlayerPower47:OnEnable() self:handleOnEnable() end
-
-function PlayerPower47:OnDisable() self:handleOnDisable() end
-
-function PlayerPower47:refreshConfig() if self:IsEnabled() then self:handleOnEnable() end end
+---Unhook, Unregister Events, Hide frames that you created.
+---You would probably only use an OnDisable if you want to
+---build a "standby" mode, or be able to toggle modules on/off.
+function PlayerPower47:OnDisable() if self.mainFrame ~= nil then self.mainFrame:Hide() end end
 
 ---@return table
 function PlayerPower47:createBar()
@@ -105,6 +108,8 @@ function PlayerPower47:createBar()
   local maxUnitPower = UnitPowerMax(self.unit)
   local percentage = ZxSimpleUI:calcPercentSafely(curUnitPower, maxUnitPower)
   self.mainFrame = self.bars:createBar(percentage)
+  self.mainFrame.DECORATIVE_NAME = self.DECORATIVE_NAME
+  self.mainFrame.frameToAnchorTo = self.bars.frameToAnchorTo
 
   self:_setRefreshColor()
   self:_registerEvents()
@@ -112,16 +117,13 @@ function PlayerPower47:createBar()
   self:_enableAllScriptHandlers()
 
   RegisterWatchHandler47:setRegisterForWatch(self.mainFrame, self.unit)
-
-  self.mainFrame:Show()
+  ZxSimpleUI.frameList[self.MODULE_NAME] = self.mainFrame
   return self.mainFrame
 end
 
----Don't have to do anything here. Maybe in the future I'll add an option to disable this bar.
-function PlayerPower47:handleEnableToggle() end
-
-function PlayerPower47:handleOnEnable()
-  if self.mainFrame ~= nil then
+function PlayerPower47:refreshConfig()
+  self:handleEnableToggle()
+  if self:IsEnabled() then
     -- If the show option is currently selected
     if self._curDbProfile.showbar == true then
       self.mainFrame.statusBar:SetStatusBarColor(unpack(self.currentPowerColorEdited))
@@ -129,11 +131,11 @@ function PlayerPower47:handleOnEnable()
       self:_setRefreshColor()
       self.bars:refreshConfig()
     end
-    self.mainFrame:Show()
   end
 end
 
-function PlayerPower47:handleOnDisable() if self.mainFrame ~= nil then self.mainFrame:Hide() end end
+---Don't have to do anything here. Maybe in the future I'll add an option to disable this bar.
+function PlayerPower47:handleEnableToggle() end
 
 -- ####################################
 -- # PRIVATE FUNCTIONS
@@ -174,7 +176,7 @@ end
 function PlayerPower47:_handlePowerChanged() self:refreshConfig() end
 
 function PlayerPower47:_registerEvents()
-  for powerEvent, _ in pairs(_powerEventColorTable) do
+  for powerEvent, _ in pairs(self._powerEventColorTable) do
     self.mainFrame:RegisterEvent(powerEvent)
   end
   -- Register Druid's shapeshift form

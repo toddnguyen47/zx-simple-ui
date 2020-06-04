632a83cc592d47768c4769d8671b563abb682dba
Todd Nguyen <todd.nguyen47@gmail.com>
2020-06-04 02:49:33 -0700

    Move functions around in InterfaceModule

--- modules/InterfaceModule.lua
+++ modules/InterfaceModule.lua
@@ -4,16 +4,24 @@ local InterfaceModule = {}
 function InterfaceModule:__init__() end
 function InterfaceModule:new() end
 
-function InterfaceModule:OnInitialization() end
-function InterfaceModule:refreshConfig() end
+---Do init tasks here, like loading the Saved Variables,
+---Or setting up slash commands.
+function InterfaceModule:OnInitialize() end
 
+---Do more initialization here, that really enables the use of your addon.
+---Register Events, Hook functions, Create Frames, Get information from
+---the game that wasn't available in OnInitialize
 function InterfaceModule:OnEnable() end
+
+---Unhook, Unregister Events, Hide frames that you created.
+---You would probably only use an OnDisable if you want to
+---build a "standby" mode, or be able to toggle modules on/off.
 function InterfaceModule:OnDisable() end
-function InterfaceModule:handleOnEnable() end
-function InterfaceModule:handleOnDisable() end
 
 function InterfaceModule:createBar() end
+function InterfaceModule:refreshConfig() end
 function InterfaceModule:handleEnableToggle() end
+
 function InterfaceModule:handleShownOption() end
 function InterfaceModule:handleShownHideOption() end
 function InterfaceModule:getExtraOptions() end

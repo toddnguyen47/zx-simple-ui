d90340e68039374bd73c5c4f502a689ba3f97a70
Todd Nguyen <todd.nguyen47@gmail.com>
2020-06-04 02:48:38 -0700

    Finish refactoring PlayerName47

--- modules/player/PlayerName47.lua
+++ modules/player/PlayerName47.lua
@@ -1,106 +1,104 @@
+-- #region
 --- upvalues to prevent warnings
 local LibStub = LibStub
 local UnitName = UnitName
 
---- include
+--- include files
 local ZxSimpleUI = LibStub("AceAddon-3.0"):GetAddon("ZxSimpleUI")
+local PlayerHealth47 = ZxSimpleUI:GetModule("PlayerHealth47")
+
 local Utils47 = ZxSimpleUI.Utils47
 local BarTemplateDefaults = ZxSimpleUI.prereqTables["BarTemplateDefaults"]
 local BarTemplate = ZxSimpleUI.prereqTables["BarTemplate"]
 local RegisterWatchHandler47 = ZxSimpleUI.RegisterWatchHandler47
 
-local _MODULE_NAME = "PlayerName47"
-local _DECORATIVE_NAME = "Player Name"
-local PlayerName47 = ZxSimpleUI:NewModule(_MODULE_NAME)
+local MODULE_NAME = "PlayerName47"
+local DECORATIVE_NAME = "Player Name"
+local PlayerName47 = ZxSimpleUI:NewModule(MODULE_NAME)
 
-PlayerName47.MODULE_NAME = _MODULE_NAME
-PlayerName47.DECORATIVE_NAME = _DECORATIVE_NAME
+PlayerName47.MODULE_NAME = MODULE_NAME
+PlayerName47.DECORATIVE_NAME = DECORATIVE_NAME
 PlayerName47.bars = nil
 PlayerName47.unit = "player"
-
-local _defaults = {
-  profile = {
-    width = 200,
-    height = 26,
-    xoffset = 400,
-    yoffset = 296,
-    fontsize = 12,
-    font = "PT Sans Bold",
-    fontcolor = {1.0, 1.0, 1.0},
-    texture = "Skewed",
-    color = {0.0, 0.0, 0.0, 1.0},
-    border = "None",
-    enabledToggle = true
-  }
-}
+-- #endregion
 
 function PlayerName47:__init__()
+  self._defaults = {
+    profile = {
+      width = 200,
+      height = 26,
+      xoffset = 0,
+      yoffset = 0,
+      relativePoint = "TOPLEFT",
+      fontsize = 14,
+      font = "PT Sans Bold",
+      fontcolor = {1.0, 1.0, 1.0},
+      texture = "Skewed",
+      color = {0.0, 0.0, 0.0, 1.0},
+      border = "None",
+      enabledToggle = true
+    }
+  }
+
+  self.mainFrame = nil
+
   self._timeSinceLastUpdate = 0
   self._prevName = UnitName(self.unit)
-  self.mainFrame = nil
 
   self._barTemplateDefaults = BarTemplateDefaults:new()
   self._newDefaults = self._barTemplateDefaults.defaults
-  Utils47:replaceTableValue(self._newDefaults.profile, _defaults.profile)
+  Utils47:replaceTableValue(self._newDefaults.profile, self._defaults.profile)
 end
 
+---Do init tasks here, like loading the Saved Variables,
+---Or setting up slash commands.
 function PlayerName47:OnInitialize()
   self:__init__()
 
-  self.db = ZxSimpleUI.db:RegisterNamespace(_MODULE_NAME, self._newDefaults)
+  self.db = ZxSimpleUI.db:RegisterNamespace(self.MODULE_NAME, self._newDefaults)
   self._curDbProfile = self.db.profile
 
   self.bars = BarTemplate:new(self.db)
-
-  self:SetEnabledState(ZxSimpleUI:getModuleEnabledState(_MODULE_NAME))
+  self:SetEnabledState(ZxSimpleUI:getModuleEnabledState(self.MODULE_NAME))
 end
 
-function PlayerName47:OnEnable() self:handleOnEnable() end
+---Do more initialization here, that really enables the use of your addon.
+---Register Events, Hook functions, Create Frames, Get information from
+---the game that wasn't available in OnInitialize
+function PlayerName47:OnEnable()
+  if self.mainFrame == nil then self:createBar() end
+  self.mainFrame:Show()
+end
 
-function PlayerName47:OnDisable() self:handleOnDisable() end
+---Unhook, Unregister Events, Hide frames that you created.
+---You would probably only use an OnDisable if you want to
+---build a "standby" mode, or be able to toggle modules on/off.
+function PlayerName47:OnDisable() if self.mainFrame ~= nil then self.mainFrame:Hide() end end
 
 ---@return table
 function PlayerName47:createBar()
   local percentage = 1.0
+
+  if PlayerHealth47.mainFrame == nil then PlayerHealth47:createBar() end
+  self.bars.frameToAnchorTo = PlayerHealth47.mainFrame
   self.mainFrame = self.bars:createBar(percentage)
+  self.mainFrame.DECORATIVE_NAME = self.DECORATIVE_NAME
+  self.mainFrame.frameToAnchorTo = self.bars.frameToAnchorTo
   self.bars:setTextOnly(self:_getFormattedName())
 
   self:_setOnShowOnHideHandlers()
   RegisterWatchHandler47:setRegisterForWatch(self.mainFrame, self.unit)
+  ZxSimpleUI.frameList[self.MODULE_NAME] = self.mainFrame
   return self.mainFrame
 end
 
 function PlayerName47:refreshConfig()
   self:handleEnableToggle()
-  if self:IsEnabled() then self:handleOnEnable() end
+  if self:IsEnabled() then self.bars:refreshConfig() end
 end
 
 function PlayerName47:handleEnableToggle()
-  ZxSimpleUI:setModuleEnabledState(_MODULE_NAME, self._curDbProfile.enabledToggle)
-end
-
-function PlayerName47:handleOnEnable()
-  if self.mainFrame ~= nil then
-    self.bars:refreshConfig()
-    self.mainFrame:Show()
-  end
-end
-
-function PlayerName47:handleOnDisable() if self.mainFrame ~= nil then self.mainFrame:Hide() end end
-
----@return table
-function PlayerName47:getExtraOptions()
-  local optionsTable = {
-    enabledToggle = {
-      -- Use parent's get/set functions
-      type = "toggle",
-      name = "Enable",
-      desc = "Enable / Disable Module `" .. _DECORATIVE_NAME .. "`",
-      order = 1,
-      width = "full"
-    }
-  }
-  return optionsTable
+  ZxSimpleUI:setModuleEnabledState(self.MODULE_NAME, self._curDbProfile.enabledToggle)
 end
 
 -- ####################################
